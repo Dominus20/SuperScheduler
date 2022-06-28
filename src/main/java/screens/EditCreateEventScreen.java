fd874568c23b119f77c89aa0c7165a99c9e74579
Dominus20 <102250448+Dominus20@users.noreply.github.com>
2022-06-28 20:58:13 +0300

    28.6.2022

--- src/main/java/screens/EditCreateEventScreen.java
+++ src/main/java/screens/EditCreateEventScreen.java
@@ -1,8 +1,11 @@
-package tests;
+package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import models.Event;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import screens.BaseScreen;
 
 public class EditCreateEventScreen extends BaseScreen {
@@ -30,6 +33,29 @@ public class EditCreateEventScreen extends BaseScreen {
 
     @FindBy(xpath = "//*[@resource-id='com.example.svetlana.scheduler:id/info_save_btn']")
     MobileElement saveBtn;
+
+    public HomeScreen createNewEvent(Event event){
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOf(title));
+        type(title, event.getTitle());
+        type(type, event.getType());
+        driver.hideKeyboard();
+
+        int breaks = event.getBrakes();
+        if(breaks>0 && breaks<5){
+            for(int i = 0; i<breaks; i++){
+                breakPlusBtn.click();
+            }
+        }
+        wageEdit.click();
+        type(wageInput, String.valueOf(event.getWage()));
+        wageSave.click();
+        //driver.hideKeyboard();
+        saveBtn.click();
+
+        return new HomeScreen(driver);
+
+    }
 
 
 
